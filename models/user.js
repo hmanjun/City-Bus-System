cd01595468fb37bfe99e5f2768d2a6db192dfcc8
hmanjun <harshith.hm03@gmail.com>
2022-08-04 14:15:54 -0700

    Fix missing exports and typos

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connections');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
@@ -42,4 +42,4 @@ User.init(
     }
 );
 
-model.exports = User;
+module.exports = User;
