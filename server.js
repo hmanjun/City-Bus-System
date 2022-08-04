ea207aff7b9ae69fa92b822be81aef164eab5c26
hmanjun <harshith.hm03@gmail.com>
2022-08-04 14:04:43 -0700

    Fix missing packages

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const session = require('session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 // const helpers = require('.utils/helpers');
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const sess = {
     secret: 'lkas dfgh fhfspk',
