c42a9418d83117c2b29e97c7c4a5fac373605b01
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-04 13:53:31 -0700

    fixed indentation on routes-model

--- models/Route.js
+++ models/Route.js
@@ -4,37 +4,38 @@ const sequelize = require('../config/connections');
 
 class Routes extends Model {}
 
-Routes.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        location_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'location',
-                key: 'id'
-            }
-        },
+Routes.init({
+
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'route'
-    });
-    
-    module.exports = Routes
-)
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    location_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'location',
+            key: 'id'
+        }
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'route'
+});
+
+module.exports = Routes
+
+
