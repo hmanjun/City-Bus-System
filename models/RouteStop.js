ffc86adac8a346297dc99dbfe97566eec69e9c4b
hmanjun <harshith.hm03@gmail.com>
2022-08-04 11:17:51 -0700

    Add RouteStop join table model

---
+++ models/RouteStop.js
@@ -0,0 +1,40 @@
+const {Model, DataTypes} = require('sequelize')
+const sequelize = require('../config/connection')
+
+class RouteStop extends Model {}
+
+RouteStop.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    route_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'route',
+            key: 'id'
+        }
+    },
+    stop_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'stop',
+            key: 'id'
+        }
+    },
+    sequence: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'routestop'
+})
