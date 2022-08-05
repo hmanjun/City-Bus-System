3175d2f4f5f07d62d00e7ce526b2c8d3bd372203
hmanjun <harshith.hm03@gmail.com>
2022-08-05 11:21:02 -0700

    Add get route names and post route

--- controllers/api/routesRoutes.js
+++ controllers/api/routesRoutes.js
@@ -1,3 +1,45 @@
 const router = require('express').Router()
+const {Route, RouteStop, Stop} = require('../../models')
+
+router.get('/:location', async (req,res) => {
+    try{
+        const routeData = await Route.findAll({
+            where: {location_id: req.params.location}
+        })
+
+        if(!routeData){
+            res.status(400).json({message: `Could not find any routes for that location`})
+            return
+        }
+
+        res.status(200).json(routeData)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.post('/:location', async (req,res) => {
+    try{
+        const {name, stops} = req.body
+        const routeData = await Route.create({
+            name,
+            location_id: req.params.location
+        })
+        /*
+        const routeStopData = stops.map((stop) => {
+            const rsData = RouteStop.create()
+        })
+        */
+        res.status(200).json(routeData)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+
+
+
+
+
 
 module.exports = router
