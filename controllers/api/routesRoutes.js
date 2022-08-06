40b06a5f61e0bc72b5ccc12c4e97189119f0e66d
hmanjun <harshith.hm03@gmail.com>
2022-08-05 17:54:32 -0700

    Add route to delete a route with id

--- controllers/api/routesRoutes.js
+++ controllers/api/routesRoutes.js
@@ -43,9 +43,51 @@ router.post('/:location', async (req,res) => {
     }
 })
 
+router.put('/:location/:id', async (req,res) => {
+    try{
+        const {name, stops} = req.body
 
+        const routeData = await Route.update({name,location_id: req.params.location}, {
+            where: {id: req.params.id}
+        })
 
+        await RouteStop.destroy({
+            where: {route_id: req.params.id}
+        })
 
+        const routeStopsArr = stops.map((stop) => {
+            return {
+                route_id: routeData.id,
+                stop_id: stop.stop_id,
+                sequence: stop.sequence
+            }
+        })
+
+        const routeStopData = await RouteStop.bulkCreate(routeStopsArr)
+
+        res.status(200).json({routeData, routeStopData})
+
+    } catch (err) {
+        res.status(200).json(err)
+    }
+})
+
+router.delete('/:location/:id', async (req,res) => {
+    try{
+        const routeData = await Route.destroy({
+            where : {id: req.params.id}
+        })
+
+        if(!routeData){
+            res.status(400).json({message: `Could not found route with that id`})
+            return
+        }
+
+        res.status(200).json({message: `Route with id ${req.params.id} was deleted`})
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
 
 
 
