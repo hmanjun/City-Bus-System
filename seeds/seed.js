54ae616331f95e174a334a02340a0d5650881ce3
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-06 11:06:59 -0700

    add function to seed in locationData

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,16 @@
-const sequelize = require('../config/connection');
-const { Loction } = require('../models');
+const sequelize = require('../config/connections');
+const { Location } = require('../models');
 
 const locationData = require('./locationData.json');
 // const seedStop = require('./stopData');
 
 const seedDatabase = async () => {
-    await sequilize.sync({ force: true });
+    await sequelize.sync({ force: true });
+
+    const locations = await Location.bulkCreate(locationData);
 
     process.exit(0);
+
 };
 
 seedDatabase();
