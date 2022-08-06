2ae6dd319b5daf6b10e00eb858f8d58b2bb2b76b
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-06 12:04:33 -0700

    add route to get homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router()
+const { Location } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const locationData = await Location.findAll()
+        const locations = await locationData.map((location) => location.get({ plain:true }));
+
+        res.render('homepage', { locations, logged_in: req.session.logged_in })
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router
