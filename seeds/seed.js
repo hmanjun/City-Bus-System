4154d2c145ca963c93cff375610c4dbba1344504
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-06 10:50:44 -0700

    add base seed.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,18 +1,13 @@
 const sequelize = require('../config/connection');
-// const { User, Project } = require('../models');
-const seedLocation = require('./locationData');
-const seedStop = require('./stopData');
+const { Loction } = require('../models');
 
-const seedAll = async () => {
+const locationData = require('./locationData.json');
+// const seedStop = require('./stopData');
+
+const seedDatabase = async () => {
     await sequilize.sync({ force: true });
-
-    // To Do:
-    // const routes = await Route.bulkCreate()
-    await seedLocation();
-    
-    await seedStop();
 
     process.exit(0);
 };
 
-seedAll();
+seedDatabase();
