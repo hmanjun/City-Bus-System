6572c98066b2ca1b0f515d584fb8a5ed3c90378b
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-07 10:42:25 -0700

    add locationPageRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router()
 const homeRoutes = require('./homeRoutes')
 const loginRoutes = require('./loginRoutes')
 const apiRoutes = require('./api')
+const locationPageRoutes = require('./locationPageRoutes')
 
 
 router.use('/', homeRoutes)
 router.use('/login',loginRoutes)
 router.use('/api',apiRoutes)
+router.use('/location', locationPageRoutes)
 
 module.exports = router;
