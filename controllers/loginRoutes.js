e75d09276e0717685ca8f09699f13179818f1a77
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-06 17:54:31 -0700

    udate get login page to send location data to handlebars

--- controllers/loginRoutes.js
+++ controllers/loginRoutes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const { Location } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('loginpage')
+        const locationData = await Location.findAll()
+        const locations = await locationData.map((location) => location.get({ plain:true }));
+        res.render('loginpage', { locations, logged_in: req.session.logged_in })
     } catch (err) {
         res.status(500).json(err);
     }
