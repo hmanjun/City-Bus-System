de7c5410aaccbf1dd1601f80307dd28534860a94
OnlyMeHere <bennett45.j.m@gmail.com>
2022-08-07 16:21:05 -0700

    add routestop seedData.json

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connections');
-const { Location, Stop } = require('../models');
+const { Location, Stop, Route, RouteStop } = require('../models');
 
 const locationData = require('./locationData.json');
 const stopData = require('./stopData.json');
+const routeData = require('./routeData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -10,6 +11,8 @@ const seedDatabase = async () => {
     const locations = await Location.bulkCreate(locationData);
 
     const stops = await Stop.bulkCreate(stopData);
+
+    const routes = await Route.bulkCreate(routeData);
 
     process.exit(0);
 
