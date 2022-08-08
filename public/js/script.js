3db6851c5b63501a1d14c8414b128aea15c8e72b
hmanjun <harshith.hm03@gmail.com>
2022-08-07 18:59:11 -0700

    Refactor route button to be listened by on click listener

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,11 @@
+//Open location data from home button click
 $(".city-btn").on("click", function () {
     const location_id = $(this).data("id")
     window.open(`/location/${location_id}`, "_self")
+})
+
+//Open route data page from click on location page
+$(".route-btn").on("click", function () {
+    const route_id = $(this).data("id")
+    window.open(`/route/${route_id}`, "_self")
 })
